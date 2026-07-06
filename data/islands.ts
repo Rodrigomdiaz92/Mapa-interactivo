557f9f6f85de1a7345ecf7d3c800fd5d15b258f9
Rodrigo <rodrigo.m.diaz@hotmail.com>
2026-07-06 12:07:08 -0300

    first commit

---
+++ data/islands.ts
@@ -0,0 +1,13 @@
+import { Island } from "@/types/island";
+
+export const islands: Island[] = [
+  {
+    id: 1,
+    name: "Isla Perro",
+    latitude: 9.572,
+    longitude: -78.898,
+    description: "Una de las islas más famosas de Guna Yala.",
+    images: [],
+    activities: ["Snorkeling", "Swimming"]
+  }
+];
---
+++ types/island.ts
@@ -0,0 +1,12 @@
+export interface Island {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+
+  description: string;
+
+  images: string[];
+
+  activities: string[];
+}
